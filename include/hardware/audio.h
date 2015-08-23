9d64192e0848ffdd5ab8f17d0ff8b3acbbbcc2d7
Epirex <estebanguzzo@gmail.com>
2015-08-23 17:27:22 -0300

    Add TARGET_NEEDS_HWC_V0 flag

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -148,6 +148,7 @@ __BEGIN_DECLS
 
 /* Set the HW synchronization source for an output stream. */
 #define AUDIO_PARAMETER_STREAM_HW_AV_SYNC "hw_av_sync"
+#define AUDIO_PARAMETER_HW_AV_SYNC "hw_av_sync"
 
 /* Query handle fm parameter*/
 #define AUDIO_PARAMETER_KEY_HANDLE_FM "handle_fm"
