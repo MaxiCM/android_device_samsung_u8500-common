edcd086dca1e41eb0d25eb150fc4ca7c9ba01a5b
Epirex <estebanguzzo@gmail.com>
2015-08-29 15:05:30 -0300

    Common: add HWUI tweaks for better perf.

--- common.mk
+++ common.mk
@@ -191,6 +191,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=1m \
     dalvik.vm.heapmaxfree=4m
 
+# HWUI tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.drop_shadow_cache_size=1
+    ro.hwui.gradient_cache_size=0.2
+    ro.hwui.layer_cache_size=6
+    ro.hwui.path_cache_size=2
+    ro.hwui.r_buffer_cache_size=1
+    ro.hwui.texture_cache_size=8
+
 # ART
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog 
